ae534e8a3600fdcf5793c840c7db2368af380772
Margaret Hannum <margie.hannum@gmail.com>
2019-10-21 19:59:29 -0400

    added first pass crr.formula

--- R/competingrisks.R
+++ R/competingrisks.R
@@ -9,11 +9,58 @@
 #' @param data MODIFY
 #' @param ... other arguments passed on to `cmprsk::crr`
 #'
+#'
 
 
 #' @export
-crr.formula <- function(...) {
-  #DO STUFF
+
+
+crr.formula <- function(formula, data, ...){
+  Call <- match.call()
+  Call[[1]] <- as.name("crr") # nicer output for user
+
+
+  # rough start at checks up top as placeholder - just want to get the formula working first
+
+  # get terms from formula input. No need for specials right now?
+  # we can add clustered competing risks functionality later maybe :-P
+  # if (is.list(formula))
+  #   Terms <- if (missing(data)) terms(formula[[1]], specials=NULL) else
+  #     terms(formula[[1]], specials=NULL, data=data)
+  # else Terms <- if (missing(data)) terms(formula, specials=NULL) else
+  #   terms(formula, specials=NULL, data=data)
+  #
+  #
+  # # create call to model.frame() that contains a formula
+  # Call$formula = formula
+  #
+  # indx <- match(c("formula", "data"),
+  #               names(Call), nomatch = 0)
+  # # check if there's a formula
+  # if (indx[1] == 0) stop("A formula argument is required, otherwise use
+  #                        standard crr input")
+
+  tTerms <- terms(formula, specials = NULL, data = data)
+  ftime_term <- tTerms[[2]][[2]]
+  fstatus_term <- tTerms[[2]][[3]]
+  form <- tTerms[[3]]
+
+  # grab vectors for input to crr
+  ftime <- data[[ftime_term]]
+  fstatus <- data[[fstatus_term]]
+
+  # covariate matrix for crr
+  cov1 <- model.matrix(as.formula(paste("~", deparse(form))), data)[, -1L]
+
+
+  fit <- crr(ftime=ftime,
+             fstatus = fstatus,
+             cov1 = cov1)
+
+  fit$terms <- tTerms
+  fit$formula <- formula(tTerms)
+
+  return(fit)
 }
 
 
--- R/riskybiz-package.R
+++ R/riskybiz-package.R
@@ -1,8 +1,13 @@
-#' @keywords internal
+#' @import cmprsk
+#' #' @keywords internal
 "_PACKAGE"
+
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+# allowing for the use of the dot when piping
+utils::globalVariables(".")
