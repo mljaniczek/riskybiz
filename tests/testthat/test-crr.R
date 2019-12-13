d7839f966cd83198a7180838a1e3a9ea6a87880b
Margaret Hannum <margie.hannum@gmail.com>
2019-12-13 13:29:49 -0500

    add tests

--- tests/testthat/test-crr.R
+++ tests/testthat/test-crr.R
@@ -1,3 +1,29 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("test-crr")
+trial <- na.omit(trial)
+covars <- model.matrix(~ age + factor(trt) + factor(grade), trial)[,-1]
+ftime1 <- trial$ttdeath
+fstatus1 <- trial$death_cr
+
+test_that("uv crr.formula no errors/warnings with standard use", {
+  expect_error(crr(Surv(ttdeath, death_cr) ~ age, trial), NA)
+  expect_warning(crr(Surv(ttdeath, death_cr) ~ age, trial), NA)
+})
+
+test_that("mv crr.formula no errors/warnings with standard use", {
+  expect_error(crr(Surv(ttdeath, death_cr) ~ age + trt + grade, trial), NA)
+  expect_warning(crr(Surv(ttdeath, death_cr) ~ age + trt + grade, trial), NA)
+})
+
+test_that("interaction crr.formula no errors/warnings with standard use", {
+  expect_error(crr(Surv(ttdeath, death_cr) ~ age*trt + grade, trial), NA)
+  expect_warning(crr(Surv(ttdeath, death_cr) ~ age*trt + grade, trial), NA)
+})
+
+test_that("mv crr.default no errors/warnings with standard use", {
+  expect_error(crr(ftime=ftime1,
+                   fstatus = fstatus1,
+                   cov1 = covars), NA)
+  expect_warning(crr(ftime=ftime1,
+                     fstatus = fstatus1,
+                     cov1 = covars), NA)
 })
